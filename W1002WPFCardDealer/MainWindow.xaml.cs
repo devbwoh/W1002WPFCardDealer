dbe6b716d73f2845903bdc553322e1f5508dd49b
BWOH <bwoh@outlook.kr>
2022-11-13 21:36:02 +0900

    랜덤 카드 1장

--- W1002WPFCardDealer/MainWindow.xaml.cs
+++ W1002WPFCardDealer/MainWindow.xaml.cs
@@ -27,8 +27,37 @@ namespace W1002WPFCardDealer
 
         private void OnDeal(object sender, RoutedEventArgs e)
         {
-            BitmapImage b = new BitmapImage(new Uri("images/10_of_clubs.png", UriKind.RelativeOrAbsolute));
+            Random r = new Random();
+
+            int n = r.Next(52);
+
+            BitmapImage b = new BitmapImage(new Uri($"images/{GetCardName(n)}",
+                    UriKind.RelativeOrAbsolute));
             Card1.Source = b;
+        }
+
+        private string GetCardName(int c)
+        {
+            string shape = "";
+            switch (c / 13)
+            {
+                case 0: shape = "spades"; break;
+                case 1: shape = "diamonds"; break;
+                case 2: shape = "hearts"; break;
+                case 3: shape = "clubs"; break;
+            }
+
+            string number = "";
+            switch (c % 13)
+            {
+                case 0: number = "ace"; break;
+                case int n when (n > 0 && n < 10):
+                    number = (c % 13 + 1).ToString(); break;
+                case 10: number = "jack"; shape += "2"; break;
+                case 11: number = "queen"; shape += "2"; break;
+                case 12: number = "king"; shape += "2"; break;
+            }
+            return string.Format("{0}_of_{1}.png", number, shape);
         }
     }
 }
